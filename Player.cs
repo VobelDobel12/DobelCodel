076e934d854be40c7d65ac1352f4438ddb297de1
VobelDobel12 <152083582+VobelDobel12@users.noreply.github.com>
2023-12-03 18:34:07 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class PlayerScript : MonoBehaviour
+ 
+public class Player : MonoBehaviour
 {
+    //Здоровье игрока
     private int health = 10;
-    private int coins = 0;
+ 
+    //Число собранных монет
+    private int coins;
+ 
+    //Префаб огненного шара и параметр Transform точки атаки
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+ 
+ 
+    //Метод, понижающий здоровье игрока
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Здоровье игрока:" + health);
-
-        if (health <= 0){
-            int index = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(index);
-        }
+        print("Здоровье игрока: " + health);
     }
-    public void CollectCoins(){
+ 
+    //Метод, увеличивающий число монеток
+    public void CollectCoins()
+    {
         coins++;
-        print("Собранные монетки:" + coins);
+        print("Собранные монетки: " + coins);
+    }
+ 
+ 
+    void Update()
+    {
+ 
+        //Если игрок кликает левой кнопкой мыши, то создаётся огненный шар
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+ 
     }
 }
