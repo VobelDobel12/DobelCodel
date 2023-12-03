b877b747acacf47a835808b9b8d759fef4273920
VobelDobel12 <152083582+VobelDobel12@users.noreply.github.com>
2023-12-03 18:34:47 +0300

    Update Coin.cs

--- Coin.cs
+++ Coin.cs
@@ -2,42 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
  
-public class Player : MonoBehaviour
+public class Coin : MonoBehaviour
 {
-    //Здоровье игрока
-    private int health = 10;
+    void OnTriggerEnter(Collider other) {
  
-    //Число собранных монет
-    private int coins;
+        Player player = other.GetComponent<Player>();
  
-    //Префаб огненного шара и параметр Transform точки атаки
-    public GameObject fireballPrefab;
-    public Transform attackPoint;
+        //Количество монеток обновляется
+        player.CollectCoins();
  
- 
-    //Метод, понижающий здоровье игрока
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-        print("Здоровье игрока: " + health);
+        //Монетка, которую собрали, уничтожается
+        Destroy(gameObject);
     }
  
-    //Метод, увеличивающий число монеток
-    public void CollectCoins()
-    {
-        coins++;
-        print("Собранные монетки: " + coins);
-    }
- 
- 
-    void Update()
-    {
- 
-        //Если игрок кликает левой кнопкой мыши, то создаётся огненный шар
-        if (Input.GetMouseButtonDown(0))
-        {
-            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
-        }
- 
-    }
 }
